0e1665effdcb2833f5030f5d9c33fb3a2065905d
Christin Jemildan Anton Yogarajah <jemildan@gmail.com>
2019-12-26 12:54:00 +0000

    added EmployeeIndexViewModel and EmployeeController

---
+++ PaySlipGenerator/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PaySlipGenerator.Models;
+using PaySlipGenerator.Services;
+
+namespace PaySlipGenerator.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeeService.GetAll().Select(employee => new EmployeeIndexViewModel
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                ImageUrl = employee.ImageUrl,
+                FullName = employee.FullName,
+                Gender = employee.Gender,
+                Designation = employee.Designation,
+                City = employee.City,
+                DateJoined = employee.DateJoined
+            }).ToList();
+            return View(employees);
+        }
+    }
+}
